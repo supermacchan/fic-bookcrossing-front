fe2ee4afe9dffbeb8db32fdeafc655534d700119
Olga Birkovska <aiba.aki9@gmail.com>
2024-01-08 18:33:48 +0200

    feat: placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <main className="text-center">Coming soon</main>;
+  return (
+    <main className="flex justify-center pt-10">
+      <Image
+        src="/images/soon.png"
+        alt="Coming soon"
+        width={500}
+        height={300}
+        loading="lazy"
+      />
+    </main>
+  );
 }
